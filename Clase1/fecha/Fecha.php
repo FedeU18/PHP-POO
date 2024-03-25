8caedbba3c314d2dfc922495ba014aaabf700249
Federico <fedee.unates.2001@gmail.com>
2024-03-25 18:11:28 -0300

    función para aumentar una fecha en un día

--- Clase1/fecha/Fecha.php
+++ Clase1/fecha/Fecha.php
@@ -6,6 +6,7 @@ class Fecha
   private $mes;
   private $meses;
   private $ano;
+  private $mesesYSusDias;
 
   public function __construct($d, $m, $a)
   {
@@ -13,19 +14,22 @@ class Fecha
     $this->mes = $m;
     $this->ano = $a;
     $this->meses = [
-      1 => "enero",
-      2 => "febrero",
-      3 => "marzo",
-      4 => "abril",
-      5 => "mayo",
-      6 => "junio",
-      7 => "julio",
-      8 => "agosto",
-      9 => "septiembre",
-      10 => "octubre",
-      11 => "noviembre",
-      12 => "diciembre",
+      1 => ["enero", 31],
+      2 => ["febrero", 28],
+      3 => ["marzo", 31],
+      4 => ["abril", 30],
+      5 => ["mayo", 31],
+      6 => ["junio", 30],
+      7 => ["julio", 31],
+      8 => ["agosto", 31],
+      9 => ["septiembre", 30],
+      10 => ["octubre", 31],
+      11 => ["noviembre", 30],
+      12 => ["diciembre", 31],
     ];
+    if (($this->ano % 4 == 0 && $this->ano % 100 != 0) || $this->ano % 400 == 0) {
+      $this->meses[2][1] = 29;
+    }
   }
 
   public function getDia()
@@ -72,13 +76,67 @@ class Fecha
    */
   public function mostrarFechaExtendida()
   {
-    $fechaExtendida = $this->getDia() . " de " . $this->getMeses()[$this->getMes()] . " de " . $this->getAno();
+    $fechaExtendida = $this->getDia() . " de " . $this->getMeses()[$this->getMes()][0] . " de " . $this->getAno();
     return $fechaExtendida;
   }
 
+  /**
+   * le aplica una suma de días a una fecha determinada
+   * @param int $dia
+   * @param string $fecha
+   * @return string
+   */
+  public function incrementarFecha($dia, $fecha)
+  {
+  }
+
+  /**
+   * incrementa un día a la fecha
+   */
+  public function incrementarUnDia($dia, $mes, $ano)
+  {
+    if ($mes == 12 && $dia == 31) {
+      $ano++;
+      $dia = 1;
+      $mes = 1;
+    } elseif ($this->comprobarCantidadDelMes($mes) == 1 && $mes != 12 && $dia == 31) {
+      $mes++;
+      $dia = 1;
+    } elseif ($this->comprobarCantidadDelMes($mes) == -1  && $dia == 30) {
+      $mes++;
+      $dia = 1;
+    } elseif ($this->comprobarCantidadDelMes($mes) == 0  && ($dia == 28 || $dia == 29)) {
+      $mes++;
+      $dia = 1;
+    } elseif ($this->comprobarCantidadDelMes($mes) == 1  && $dia < 31) {
+      $dia++;
+    } elseif ($this->comprobarCantidadDelMes($mes) == -1  && $dia < 30) {
+      $dia++;
+    } elseif ($this->comprobarCantidadDelMes($mes) == -1  && ($dia < 28 || $dia < 29)) {
+      $dia++;
+    }
+
+    return "$dia/$mes/$ano";
+  }
+
+  /**
+   * retorna 1 si el mes tiene 31 dias, -1 si el mes tiene 30 y retorna 0 si el mes es febrero
+   */
+  public function comprobarCantidadDelMes($mesConsultado)
+  {
+    $diasDelMes = 0;
+    if ($this->getMeses()[$mesConsultado][1] == 31) {
+      $diasDelMes = 1;
+    } else if ($this->getMeses()[$mesConsultado][1] == 30) {
+      $diasDelMes = -1;
+    }
+    return $diasDelMes;
+  }
+
+
   public function __toString()
   {
-    return "la fecha es: " . $this->mostrarFechaAbreviada() . "\n\n\n";
+    return "\nla fecha es: " . $this->mostrarFechaAbreviada() . "\n\n\n";
   }
   public function __destruct()
   {
--- Clase1/fecha/Test_Fecha.php
+++ Clase1/fecha/Test_Fecha.php
@@ -4,7 +4,14 @@ include "Fecha.php";
 
 $objFecha = new Fecha(5, 7, 2001);
 
-echo $objFecha->mostrarFechaAbreviada() . "\n";
-echo $objFecha->mostrarFechaExtendida() . "\n";
+// echo $objFecha->mostrarFechaAbreviada() . "\n";
+// echo $objFecha->mostrarFechaExtendida() . "\n";
+
+echo "\n\n\n" . $objFecha->incrementarUnDia(30, 11, 2001) . "\n\n\n";
+echo "\n\n\n" . $objFecha->incrementarUnDia(31, 12, 2001) . "\n\n\n";
+echo "\n\n\n" . $objFecha->incrementarUnDia(29, 2, 2024) . "\n\n\n";
+echo "\n\n\n" . $objFecha->incrementarUnDia(2, 2, 2024) . "\n\n\n";
+echo "\n\n\n" . $objFecha->incrementarUnDia(15, 7, 2024) . "\n\n\n";
+echo "\n\n\n" . $objFecha->incrementarUnDia(31, 7, 2024) . "\n\n\n";
 
 echo $objFecha;
